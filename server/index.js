07ccb1437fc68ca66506c8cd23fb5c4cb3d721f1
Martin <MartinGlyer@gmail.com>
2018-10-23 10:01:21 -0400

    adds graphql middleware

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const server = express()
+const graphqlHTTP = require('express-graphql')
 
-app.listen(8084, () => console.log('now listening on port 8084'))
+server.use('/graphql', graphqlHTTP({
+    
+}))
+
+server.listen(8084, () => console.log('now listening on port 8084'))
