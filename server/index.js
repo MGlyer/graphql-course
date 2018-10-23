4258387d6a82d6407949eb35dc8e00770c05a264
Martin <MartinGlyer@gmail.com>
2018-10-23 10:37:55 -0400

    Creates RootQuery and exports Schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const server = express()
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 server.use('/graphql', graphqlHTTP({
-    
+    schema
 }))
 
 server.listen(8084, () => console.log('now listening on port 8084'))
