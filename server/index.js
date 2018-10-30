82fcd0fd95cdab575eb99fca1dc52165629c6eb5
Martin <MartinGlyer@gmail.com>
2018-10-30 19:42:51 -0400

    connects to mlab

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const server = express()
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://guest:p4ssword@ds147073.mlab.com:47073/graphql-course')
+let db = mongoose.connection
+
+db.once('open', () => {
+    console.log('connected to mlab')
+})
 server.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
