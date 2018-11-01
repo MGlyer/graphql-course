739d291d5bd8d6139cdf66d8e8b882610ea17148
Martin <MartinGlyer@gmail.com>
2018-11-01 19:07:18 -0400

    writes change functions and author select

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -4,7 +4,22 @@ import { graphql } from 'react-apollo'
 
 class AddBook extends React.Component {
   state = {
-    //
+    selectedAuthor: '',
+    bookName: '',
+    genreName: ''
+  }
+  changeAuthor = this.changeAuthor.bind(this)
+  handleTextChange = this.handleTextChange.bind(this)
+
+  changeAuthor(e) {
+    e.preventDefault
+    this.setState({selectedAuthor: e.target.value})
+  }
+
+  handleTextChange(e) {
+    e.preventDefault
+    let key = e.target.className
+    this.setState({[key]: e.target.value})
   }
 
   render() {
@@ -13,19 +28,28 @@ class AddBook extends React.Component {
         <form>
 
           <div className="field">
-            <label>Book name:</label>
-            <input type="text"/>
+            <label>Book name:  </label>
+            <input type="text" className="bookName" onChange={this.handleTextChange} />
           </div>
 
           <div className="field">
-            <label>Genre:</label>
-            <input type="text"/>
+            <label>Genre:  </label>
+            <input type="text" className="genreName" onChange={this.handleTextChange}/>
           </div>
 
           <div className="field">
-            <label>Author:</label>
-            <select>
+            <label>Author:  </label>
+            <select onChange={this.changeAuthor}>
               <option>Select Author</option>
+              {this.props.data.loading ?
+              <option>loading authors...</option>
+              :
+              this.props.data.authors.map((author) => {
+                return(
+                  <option value={author.name}>{author.name}</option>
+                )
+              })}
+              
             </select>
           </div>
         </form>
