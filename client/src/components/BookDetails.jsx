f5a00fa9af5b49ff8f6eb4f6869a9cc14da4a771
Martin <MartinGlyer@gmail.com>
2018-11-07 16:48:17 -0500

    full function

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -9,14 +9,30 @@ class BookDetails extends React.Component {
 
   render() {
     var book = this.props.data.book || null
-    return (
-      <div>
-        {this.props.data.book === null ? 
-          null :
-          <p>Book Name: {book.name}</p>
-        }
-      </div>
-    )
+    if (book === null) {
+      return(
+        null
+      )
+    } else {
+      return(
+        <div>
+          <p>Book Name: {book.name} </p>
+          <p>Genre: {book.genre} </p>
+          <p>Author:  {book.author.name} </p>
+          <p>Author Age: {book.author.age} </p>
+          <div>
+            <p>Other books by this author: </p>
+            <ul>
+              {book.author.book.map((otherBook) => {
+                return(
+                  <li>{otherBook.name}</li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      )
+    }
   }
 
 }
